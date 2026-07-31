c4dfa59207a5f9c598173f46e3caa2704766884e
Rafiul Alam Antar <rafiulantar@gmail.com>
2026-07-31 22:25:42 +0600

    extracting the text from browser and inserting valid expect asserstions in test

--- playwright.config.js
+++ playwright.config.js
@@ -9,7 +9,7 @@ const config = ({
   testDir: './tests',
   timeout: 40 * 1000,
   expect: {
-    timeout: 40*1000
+    timeout: 5000
   },
   reporter: 'html',
   /* Run tests in files in parallel */
--- tests/UIBascistest.spec.js
+++ tests/UIBascistest.spec.js
@@ -2,22 +2,26 @@ const {test} = require('@playwright/test');
 const {expect} = require('@playwright/test');
 
 
-test('First Playwright Test with browser context', async ({browser}) =>
+test.only('First Playwright Test with browser context', async ({browser}) =>
 {
     const context =  await browser.newContext();
     const page =  await context.newPage();
     await page.goto(process.env.BASE_URL);
     console.log(await page.title());
-    expect(page).toHaveTitle('LoginPage');
+    expect(page).toHaveTitle('LoginPage Practise | Rahul Shetty Academy');
     await page.locator("#username").fill(process.env.TEST_USERNAME);
     await page.locator("#password").fill(process.env.TEST_PASSWORD);
-    await page.locator("#signInBtn").click();
+    console.log(await page.locator("#signInBtn").click());
+    console.log(await page.locator("[style*='block']").textContent());
+    await expect(page.locator("[style*='block']")).toContainText('Incorrect username/password.');
+
+
 
 
 });
 test('First Playwright Test with page', async ({page}) =>
 {
     await page.goto(process.env.BASE_URL);
-    expect(await page.title()).toBe('LoginPage');
+    expect(await page.title()).toBe('LoginPage Practise | Rahul Shetty Academy');
 
 });
