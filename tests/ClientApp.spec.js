4e950c47ecf53533ef94be6e3ba84cb13c74494f
Rafiul Alam Antar <rafiulantar@gmail.com>
2026-08-02 23:47:09 +0600

    Write the Script to dynamically find the product to buy from list of products

--- tests/ClientApp.spec.js
+++ tests/ClientApp.spec.js
@@ -4,6 +4,9 @@ const {expect} = require('@playwright/test');
 
 test('Browser Context Test', async ({page}) =>
 {
+    const productsNames = "Zara Coat 4";
+    const products = page.locator(".card-body");
+
     await page.goto(process.env.BASE_URL_CLIENT_APP);
     await page.locator("#userEmail").fill(process.env.TEST_EMAIL);
     await page.locator("#userPassword").fill(process.env.TEST_PASSWORD_CLIENT_APP);
@@ -13,5 +16,18 @@ test('Browser Context Test', async ({page}) =>
     await page.locator(".card-body b").allTextContents();
     const titles = await page.locator(".card-body b").allTextContents();
     console.log(titles);
+
+
+
+    //select product Zara Coat 4 and click on the add to cart button
+    const productCount = await products.count();
+    console.log(productCount);
+    for (let i = 0; i < productCount; ++i){
+        if (await products.nth(i).locator("b").textContent() === productsNames) {
+            await products.nth(i).locator("text= Add To Cart").click();
+            break;
+        }
+
+    }
 
 });
