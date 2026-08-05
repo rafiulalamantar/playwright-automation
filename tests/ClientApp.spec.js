45a48c99539976f81c5e596f940e66877fc17c14
Rafiul Alam Antar <rafiulantar@gmail.com>
2026-08-05 13:15:28 +0600

    Dynamically find the order from OrderHistory page using Playwright Script logic

--- tests/ClientApp.spec.js
+++ tests/ClientApp.spec.js
@@ -2,7 +2,7 @@ const {test} = require('@playwright/test');
 const {expect} = require('@playwright/test');
 
 
-test('Client App test with submit order and extract order ID', async ({page}) =>
+test.only('Client App test with submit order and extract order ID', async ({page}) =>
 {
     const productsNames = "ZARA COAT 3";
     const products = page.locator(".card-body");
@@ -49,7 +49,25 @@ test('Client App test with submit order and extract order ID', async ({page}) =>
     expect(await page.locator(".user__name [type='text']").first()).toHaveText(process.env.TEST_EMAIL);
     await page.locator(".action__submit").click();
     await expect(page.locator(".hero-primary")).toHaveText(" Thankyou for the order. ");
-    const orderId = await page.locator(".em-spacer-1 .ng-star-inserted").textContent();
+    // const orderId = await page.locator(".em-spacer-1 .ng-star-inserted").textContent();
+
+    let orderId = await page.locator(".em-spacer-1 .ng-star-inserted").textContent();
+    orderId = orderId.replace(/\|/g, "").trim();
     console.log(orderId);
+    await page.locator("button[routerlink*='myorders']").click();
+    await page.locator("tbody").waitFor();
+    const rows = await page.locator("tbody tr");
+    for (let i = 0; i < await rows.count(); ++i){
+        const rowOrderId = (await rows.nth(i).locator("th").textContent()).trim();
+        if (orderId.includes(rowOrderId)) {
+            await rows.nth(i).locator("button").first().click();
+            break;
+        }
+    }
+    // const orderDetails = await expect(page.locator(".col-text")).toHaveText(orderId);
+    // await expect(orderDetails.first()).toHaveText(orderId);
+    
+    const orderDetails = page.locator(".col-text", { hasText: orderId });
+    await expect(orderDetails.first()).toHaveText(orderId);
 
 });
