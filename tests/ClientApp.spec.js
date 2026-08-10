a5bf740ef33db097a199ec693639e3b22ca2ddfa
Rafiul Alam Antar <rafiulantar@gmail.com>
2026-08-10 22:43:21 +0600

    Implement Page Object Model for Cart and Orders functionality; enhance DashboardPage with order navigation

--- tests/ClientApp.spec.js
+++ tests/ClientApp.spec.js
@@ -78,58 +78,25 @@ test.only('Client App test with submit order and extract order ID using Page Obj
    const poManager = new POManager(page);
     const productsNames = "ZARA COAT 3";
     const email = "fijope2288@amupx.com";
-   //  const products = page.locator(".card-body");
 
     const loginPage = poManager.getLoginPage();
     await loginPage.goToLoginPage();
     await loginPage.validateLoginPage(process.env.TEST_EMAIL, process.env.TEST_PASSWORD);
-    // await page.waitForLoadState('networkidle');
     const dashboard = poManager.getDashboardPage();
     await dashboard.searchProductAndAddToCart(productsNames);
     await dashboard.navigateToCart();
 
+    const cartPage = poManager.getCartPage();
+    await cartPage.VerifyProductIsDisplayed(productsNames);
+    await cartPage.Checkout();
 
-
-
-   //  await page.locator("[routerlink*='cart']").click();
-   // //await page.pause();
- 
-   // await page.locator("div li").first().waitFor();
-   // const bool = await page.locator("h3:has-text('ZARA COAT 3')").isVisible();
-   // expect(bool).toBeTruthy();
-   // await page.locator("text=Checkout").click();
- 
-   // await page.locator("[placeholder*='Country']").pressSequentially("ind", { delay: 150 });
-   // const dropdown = page.locator(".ta-results");
-   // await dropdown.waitFor();
-   // const optionsCount = await dropdown.locator("button").count();
-   // for (let i = 0; i < optionsCount; ++i) {
-   //    const text = await dropdown.locator("button").nth(i).textContent();
-   //    if (text === " India") {
-   //       await dropdown.locator("button").nth(i).click();
-   //       break;
-   //    }
-   // }
- 
-   // expect(page.locator(".user__name [type='text']").first()).toHaveText(email);
-   // await page.locator(".action__submit").click();
-   // await expect(page.locator(".hero-primary")).toHaveText(" Thankyou for the order. ");
-   // const orderId = await page.locator(".em-spacer-1 .ng-star-inserted").textContent();
-   // console.log(orderId);
- 
-   // await page.locator("button[routerlink*='myorders']").click();
-   // await page.locator("tbody").waitFor();
-   // const rows = await page.locator("tbody tr");
- 
- 
-   // for (let i = 0; i < await rows.count(); ++i) {
-   //    const rowOrderId = await rows.nth(i).locator("th").textContent();
-   //    if (orderId.includes(rowOrderId)) {
-   //       await rows.nth(i).locator("button").first().click();
-   //       break;
-   //    }
-   // }
-   // const orderIdDetails = await page.locator(".col-text").textContent();
-   // expect(orderId.includes(orderIdDetails)).toBeTruthy();
+    const ordersReviewPage = poManager.getOrdersReviewPage();
+    await ordersReviewPage.searchCountryAndSelect("ind","India");
+    const orderId = await ordersReviewPage.SubmitAndGetOrderId();
+   console.log(orderId);
+   await dashboard.navigateToOrders();
+   const ordersHistoryPage = poManager.getOrdersHistoryPage();
+   await ordersHistoryPage.searchOrderAndSelect(orderId);
+   expect(orderId.includes(await ordersHistoryPage.getOrderId())).toBeTruthy();
  
 });
