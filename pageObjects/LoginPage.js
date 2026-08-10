eed0e0ef11d368740a35b27445d4f6ef0ea2c4af
Rafiul Alam Antar <rafiulantar@gmail.com>
2026-08-10 21:54:26 +0600

    Creating Page objects and action methods for end to end Script - Part 1

--- pageObjects/LoginPage.js
+++ pageObjects/LoginPage.js
@@ -1,3 +1,4 @@
+
 class LoginPage {
 
     constructor(page) {
@@ -13,8 +14,9 @@ class LoginPage {
     async validateLoginPage(username, password) {
 
         await this.emailField.fill(process.env.TEST_EMAIL);
-        await this.passwordField.fill(process.env.TEST_PASSWORD_CLIENT_APP);
+        await this.passwordField.fill(process.env.TEST_PASSWORD);
         await this.signInButton.click();
+        await this.page.waitForLoadState('networkidle');
     }
 
 }
--- tests/ClientApp.spec.js
+++ tests/ClientApp.spec.js
@@ -1,6 +1,7 @@
 const {test} = require('@playwright/test');
 const {expect} = require('@playwright/test');
 const {LoginPage} = require('../pageObjects/LoginPage');
+const { DashboardPage } = require('../pageObjects/DashboardPage');
 
 test('Client App test with submit order and extract order ID', async ({page}) =>
 {
@@ -10,7 +11,7 @@ test('Client App test with submit order and extract order ID', async ({page}) =>
 
     const loginPage = new LoginPage(page);
     await loginPage.goToLoginPage();
-    loginPage.validateLoginPage(process.env.TEST_EMAIL, process.env.TEST_PASSWORD_CLIENT_APP);
+    await loginPage.validateLoginPage(process.env.TEST_EMAIL, process.env.TEST_PASSWORD_CLIENT_APP);
     // await page.waitForLoadState('networkidle');
     await page.locator(".card-body b").first().waitFor();// when locator returns single element
     await page.locator(".card-body b").allTextContents();
@@ -69,5 +70,64 @@ test('Client App test with submit order and extract order ID', async ({page}) =>
    }
    const orderIdDetails = await page.locator(".col-text").textContent();
    expect(orderId.includes(orderIdDetails)).toBeTruthy();
+ 
+});
+test.only('Client App test with submit order and extract order ID using Page Object', async ({page}) =>
+{
+    const productsNames = "ZARA COAT 3";
+    const email = "fijope2288@amupx.com";
+   //  const products = page.locator(".card-body");
+
+    const loginPage = new LoginPage(page);
+    await loginPage.goToLoginPage();
+    await loginPage.validateLoginPage(process.env.TEST_EMAIL, process.env.TEST_PASSWORD);
+    // await page.waitForLoadState('networkidle');
+    const dashboard = new DashboardPage(page);
+    await dashboard.searchProductAndAddToCart(productsNames);
+    await dashboard.navigateToCart();
+
+
+
+
+   //  await page.locator("[routerlink*='cart']").click();
+   // //await page.pause();
+ 
+   // await page.locator("div li").first().waitFor();
+   // const bool = await page.locator("h3:has-text('ZARA COAT 3')").isVisible();
+   // expect(bool).toBeTruthy();
+   // await page.locator("text=Checkout").click();
+ 
+   // await page.locator("[placeholder*='Country']").pressSequentially("ind", { delay: 150 });
+   // const dropdown = page.locator(".ta-results");
+   // await dropdown.waitFor();
+   // const optionsCount = await dropdown.locator("button").count();
+   // for (let i = 0; i < optionsCount; ++i) {
+   //    const text = await dropdown.locator("button").nth(i).textContent();
+   //    if (text === " India") {
+   //       await dropdown.locator("button").nth(i).click();
+   //       break;
+   //    }
+   // }
+ 
+   // expect(page.locator(".user__name [type='text']").first()).toHaveText(email);
+   // await page.locator(".action__submit").click();
+   // await expect(page.locator(".hero-primary")).toHaveText(" Thankyou for the order. ");
+   // const orderId = await page.locator(".em-spacer-1 .ng-star-inserted").textContent();
+   // console.log(orderId);
+ 
+   // await page.locator("button[routerlink*='myorders']").click();
+   // await page.locator("tbody").waitFor();
+   // const rows = await page.locator("tbody tr");
+ 
+ 
+   // for (let i = 0; i < await rows.count(); ++i) {
+   //    const rowOrderId = await rows.nth(i).locator("th").textContent();
+   //    if (orderId.includes(rowOrderId)) {
+   //       await rows.nth(i).locator("button").first().click();
+   //       break;
+   //    }
+   // }
+   // const orderIdDetails = await page.locator(".col-text").textContent();
+   // expect(orderId.includes(orderIdDetails)).toBeTruthy();
  
 });
